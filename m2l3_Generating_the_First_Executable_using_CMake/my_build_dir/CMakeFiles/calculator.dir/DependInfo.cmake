f21f01615e163a1ab55f4b41d92e4d339804d734
SubrataBauri <subrata@digitaptech.com>
2021-05-12 21:59:43 +0000

    Generating the first executable using CMake

---
+++ m2l3_Generating_the_First_Executable_using_CMake/my_build_dir/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m2l3_Generating_the_First_Executable_using_CMake/addition.cpp" "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m2l3_Generating_the_First_Executable_using_CMake/my_build_dir/CMakeFiles/calculator.dir/addition.cpp.o"
+  "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m2l3_Generating_the_First_Executable_using_CMake/division.cpp" "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m2l3_Generating_the_First_Executable_using_CMake/my_build_dir/CMakeFiles/calculator.dir/division.cpp.o"
+  "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m2l3_Generating_the_First_Executable_using_CMake/main.cpp" "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m2l3_Generating_the_First_Executable_using_CMake/my_build_dir/CMakeFiles/calculator.dir/main.cpp.o"
+  "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m2l3_Generating_the_First_Executable_using_CMake/print_result.cpp" "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m2l3_Generating_the_First_Executable_using_CMake/my_build_dir/CMakeFiles/calculator.dir/print_result.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
