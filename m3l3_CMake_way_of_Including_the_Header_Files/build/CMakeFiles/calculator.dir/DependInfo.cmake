6cd9f8510a958bf20f677e58b236c6b9dd6cefc1
SubrataBauri <subrata@digitaptech.com>
2021-05-13 17:42:33 +0000

    CMake way of Including the Header files in Subdirectories

---
+++ m3l3_CMake_way_of_Including_the_Header_Files/build/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m3l3_CMake_way_of_Including_the_Header_Files/main.cpp" "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m3l3_CMake_way_of_Including_the_Header_Files/build/CMakeFiles/calculator.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../my_math_dir/include"
+  "../my_print_dir/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m3l3_CMake_way_of_Including_the_Header_Files/build/my_math_dir/CMakeFiles/my_math.dir/DependInfo.cmake"
+  "/home/conan/CMake_for_Cross-Platform_CXX_Project_Building/m3l3_CMake_way_of_Including_the_Header_Files/build/my_print_dir/CMakeFiles/my_print.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
